953c9351f375c76669e26660d837af97b678fb8a
Derek Ogle <dogle@northland.edu>
2017-08-01 21:05:23 -0500

    Edits to Data_Prepper
    Mostly dealth with database issues

--- code/Data_Prepper.R
+++ code/Data_Prepper.R
@@ -13,19 +13,28 @@
 # THE PREPPED FILES IN 'data/original/'.
 
 # However, as a safeguard, the files will only be overwritten if the
-# following object is set to TRUE.
-writePreppedFiles <- TRUE
+# writePreppedFiles object below is set to TRUE.
+#
 # ======================================================================
 # ######################################################################
 
 
 
 # ======================================================================
-# clear workspace and console and load required packages
-library(FSA)
-library(dplyr)
+# Setup
+## Clear workspace and console
 rm(list=ls())
 cat("\014")
+##  Load required packages
+library(FSA)
+library(dplyr)
+library(magrittr)
+## Set whether old prepped files should be over-written (see above)
+writePreppedFiles <- TRUE
+## Set the random number seed. Expanding the lengths of the fish
+## generates a random length between the lower and upper lengths of the
+## length bin. This help keep some constancy in that randomization.
+set.seed(82340934)
 
 
 
@@ -39,47 +48,73 @@ PE <- read.csv("data/original/walleye_pe_data.csv") %>%
   mutate(wbic_year=paste(wbic,year,sep="_")) %>%
   select(wbic_year,wbic,year,PE) %>%
   arrange(wbic,year)
-str(PE)
-headtail(PE)
+## Removed wbics that ended in 1 (these are lake chains) ... check this
+##   by running first two lines below and PE[(PE$wbic %% 10)==1,]
+tmp <- nrow(PE)
+PE %<>% filterD(!(wbic %% 10)==1)
+cat("Removing lake chains deleted",tmp-nrow(PE),"WBIC_YEAR PE values.\n")
 if (writePreppedFiles) write.csv(PE,"data/prepped/PE.csv",row.names=FALSE)
 
-# Found unique wbic codes in the PE file.
+# Found unique wbic and wbic_year codes in the PE file.
 ## We must have a PE to estimate P and B. Thus, these wbic codes can be
 ## used to filter down the other (ROW, FMDB, etc.) files so that we
 #  limit memory issues
 wbics <- unique(PE$wbic)
-
+wbic_years <- unique(PE$wbic_year)
 rm(PE)
 
 
 
 # ======================================================================
 # Read and prep the FMDB data file
-## Reduced to only WBICs for which we have a PE
 ## Selected only needed columns, renamed those columns
-## Removed fish for which lengths were not recorded
-## Generated lengths for each fish that was recorded in a bin of lengths
-## Added length in mm and wbic-year combination variables
-## Rearranged columns and sorted rows by wbic, year, and length
-fmdb <- read.csv("data/original/walleye_fmdb_raw_2012.csv",
+## Reduced to only WBIC_YEARs for which we have a PE
+fmdb <- read.csv("data/original/walleye_raw_2012.csv",
                  stringsAsFactors=FALSE,na.strings=c("-","NA","")) %>%
-  filterD(WBIC %in% wbics) %>%
-  select(WBIC,Station.Name,Survey.Year,Sample.Date,
-         Length.or.Lower.Length.IN,Length.Upper.IN,Number.of.Fish,Gender) %>%
-  rename(wbic=WBIC,station=Station.Name,year=Survey.Year,
-         date=Sample.Date,sex=Gender) %>%
-  #### Short-term work-around for data entry error, ultimately delete this??
-  filterD(!is.na(Number.of.Fish),!Number.of.Fish<1) %>%
-  ####
-  #### Does this make sense ... what to do with fish with no lengths
-  filterD(!is.na(Length.or.Lower.Length.IN)) %>%
-  ####
-  expandCounts(~Number.of.Fish,
-               ~Length.or.Lower.Length.IN+Length.Upper.IN,
-               new.name="len.in") %>%
-  mutate(len.mm=len.in*25.4,
-         wbic_year=paste(wbic,year,sep="_")) %>%
-  select(wbic_year,wbic,year,station,date,len.in,len.mm,sex,lennote) %>%
+  select(WBIC,Survey.Year,Length.or.Lower.Length.IN,Length.Upper.IN,
+         Number.of.Fish,Gender) %>%
+  rename(wbic=WBIC,year=Survey.Year,sex=Gender) %>%
+  mutate(wbic_year=paste(wbic,year,sep="_")) %>%
+  filterD(wbic_year %in% wbic_years)
+## Handling database errors or issues (per AR e-mail 1-Aug-17)
+## Don't use filterD() because it removes NAs which causes issues
+## Removed rows where a number of fish is given, but no lengths
+rows2delete <- which(fmdb$Number.of.Fish>0 & is.na(fmdb$Length.or.Lower.Length.IN))
+if (length(rows2delete)>0) fmdb <- fmdb[-rows2delete,]
+cat("Deleted",length(rows2delete),"rows from the FMDB with fish but no length.\n")
+## Removed rows with "lower" length greater than "upper" length
+rows2delete <- which(fmdb$Length.or.Lower.Length.IN>fmdb$Length.Upper.IN)
+if (length(rows2delete)>0) fmdb <- fmdb[-rows2delete,]
+cat("Deleted",length(rows2delete),"rows from the FMDB with lower length > upper length.\n")
+### Removed rows with negative lengths
+rows2delete <- which(fmdb$Length.or.Lower.Length.IN<0 | fmdb$Length.Upper.IN<0)
+if (length(rows2delete)>0) fmdb <- fmdb[-rows2delete,]
+cat("Deleted",length(rows2delete),"rows from the FMDB with negative lengths.\n")
+## Removed rows where a number of fish is given, but the lower length
+## is zero and the upper length is positive. I think this is for fish
+## that were counted as less than the upper length. Two of the upper
+## lengths were 50 which were likely typos, one was 12, two were 9,
+## and the rest were <6.9. Only the 12 would likely come close to
+## affecting P calculations, but there is no way to assign realistic
+## lengths to these fish (because will be between 0 and 12).
+rows2delete <- which(fmdb$Length.or.Lower.Length.IN==0 & 
+                       fmdb$Length.Upper.IN>0 & fmdb$Number.of.Fish>0)
+if (length(rows2delete)>0) fmdb <- fmdb[-rows2delete,]
+cat("Deleted",length(rows2delete),"rows from the FMDB in a 'small fish' bin.\n")
+## If no or zero numbers of fish but there are lengths then 
+## assume there is one fish.
+fmdb$Number.of.Fish[is.na(fmdb$Number.of.Fish) & 
+                      !is.na(fmdb$Length.or.Lower.Length.IN)] <- 1
+fmdb$Number.of.Fish[fmdb$Number.of.Fish==0 & 
+                      !is.na(fmdb$Length.or.Lower.Length.IN)] <- 1
+## Generated individual lengths for fish recorded in length bins
+## Added a length in mm variable
+## Rearranged the columns and sorted the rows by wbic, year, and length
+fmdb %<>% expandCounts(~Number.of.Fish,
+                       ~Length.or.Lower.Length.IN+Length.Upper.IN,
+                       new.name="len.in") %>%
+  mutate(len.mm=len.in*25.4) %>%
+  select(wbic_year,wbic,year,len.in,len.mm,sex,lennote) %>%
   arrange(wbic,year,len.mm)
 str(fmdb)
 headtail(fmdb)
@@ -98,16 +133,16 @@ rm(fmdb)
 ## Rearranged columns and sorted rows by wbic, year, length, and age
 lwa <- read.csv("data/original/walleye_length_weight_age.csv",
                 stringsAsFactors=FALSE,na.strings=c("-","NA","")) %>%
-  filterD(WBIC %in% wbics) %>%
-  select(WBIC,Waterbody.Name,Survey.Year,Sample.Date,Length.IN,
-         Weight.Grams,Age..observed.annuli.,Age.Structure.,Gender) %>%
-  rename(wbic=WBIC,name=Waterbody.Name,year=Survey.Year,
-         date=Sample.Date,sex=Gender,len.in=Length.IN,wt=Weight.Grams,
+  select(WBIC,Survey.Year,Length.IN,Weight.Grams,
+         Age..observed.annuli.,Age.Structure.,Gender) %>%
+  rename(wbic=WBIC,year=Survey.Year,sex=Gender,
+         len.in=Length.IN,wt=Weight.Grams,
          age=Age..observed.annuli.,strux=Age.Structure.) %>%
   mutate(len.mm=len.in*25.4,
          wt=as.numeric(gsub(',','',wt)),
          wbic_year=paste(wbic,year,sep="_")) %>%
-  select(wbic_year,wbic,year,date,name,len.in,len.mm,wt,age,sex,strux) %>%
+  filterD(wbic_year %in% wbic_years) %>%
+  select(wbic_year,wbic,year,len.in,len.mm,wt,age,sex,strux) %>%
   arrange(wbic,year,len.mm,age)
 
 # Isolate those fish that have both length and weight
@@ -135,6 +170,7 @@ rm(lwa,lw,la)
 ## Selected only needed columns, renamed those columns
 ## Converted lake size and depths to metrics units (ha, m)
 ## Rearranged columns and sorted rows by wbic
+## !! NEED TO CREATE SOME SORT OF REGION VARIABLE (for ALK and LW)
 ROW <- read.csv("data/original/ROW.csv") %>%
   filterD(WBIC %in% wbics) %>%
   select(WBIC,WBIC_TYPE_CODE,OFFICIAL_NAME,LOCAL_NAME,
@@ -156,12 +192,4 @@ wbics[which(!wbics %in% ROW$wbic)]
 if (writePreppedFiles) write.csv(ROW,"data/prepped/ROW.csv",row.names=FALSE)
 rm(ROW)
 
-
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# Possibly use this extract wbic,county pair information that you may ultimately join with the ROW data.frame.
-#tmp <- read.csv("data/original/walleye_fmdb_raw_2012.csv",
-#                stringsAsFactors=FALSE,na.strings=c("-","NA","")) %>%
-#  filterD(WBIC %in% wbics) %>%
-#  select(WBIC,County) %>%
-#  unique()
 
