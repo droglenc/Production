d5bde7494e9302be690d181a2996d4c45d770059
Derek Ogle <dogle@northland.edu>
2017-08-03 17:20:41 -0500

    Updated Data_Prepper
    Added lake classifications, create a logger

--- code/Data_Prepper.R
+++ code/Data_Prepper.R
@@ -1,6 +1,6 @@
 # ######################################################################
 # ======================================================================
-# These scripts read the original data files as delivered to us from the
+# This script read the original data files as delivered to us from the
 # WiDNR database and located in 'data/originals/'. The data files are 
 # then manipulated, mostly to reduce the number of unneeded variables
 # (columns), remove unneeded records (rows), and create new veriables
@@ -13,7 +13,10 @@
 # THE PREPPED FILES IN 'data/original/'.
 
 # However, as a safeguard, the files will only be overwritten if the
-# writePreppedFiles object below is set to TRUE.
+# writePreppedFiles object below is set to TRUE. If writePreppedFiles=
+# TRUE, then a log will be written in the prepped files folder. The log
+# should be carefully compared to old logs to determine if changes
+# caused any major issues (too few variables, too few records, etc.)
 #
 # ======================================================================
 # ######################################################################
@@ -27,10 +30,14 @@ rm(list=ls())
 cat("\014")
 ##  Load required packages
 library(FSA)
+library(plyr)
 library(dplyr)
 library(magrittr)
 ## Set whether old prepped files should be over-written (see above)
 writePreppedFiles <- TRUE
+## Initialize log variable
+log <- c(paste0("Ran Data Prepper to create new prepped files on ",
+                date(),".","\n","\n"))
 ## Set the random number seed. Expanding the lengths of the fish
 ## generates a random length between the lower and upper lengths of the
 ## length bin. This help keep some constancy in that randomization.
@@ -43,8 +50,10 @@ set.seed(82340934)
 ## Renamed variables (preference and consistency)
 ## Added wbic-year combination variable
 ## Reordered variables and sorted rows by wbic and year (preference)
-PE <- read.csv("data/original/walleye_pe_data.csv") %>%
-  rename(wbic=WBIC,year=Year) %>%
+PE <- read.csv("data/original/walleye_pe_data.csv")
+log <- c(log,paste("Loaded PE.csv with",ncol(PE),"variables and",
+                 nrow(PE),"records."))
+PE %<>% rename(wbic=WBIC,year=Year) %>%
   mutate(wbic_year=paste(wbic,year,sep="_")) %>%
   select(wbic_year,wbic,year,PE) %>%
   arrange(wbic,year)
@@ -52,8 +61,8 @@ PE <- read.csv("data/original/walleye_pe_data.csv") %>%
 ##   by running first two lines below and PE[(PE$wbic %% 10)==1,]
 tmp <- nrow(PE)
 PE %<>% filterD(!(wbic %% 10)==1)
-cat("Removing lake chains deleted",tmp-nrow(PE),"WBIC_YEAR PE values.\n")
-if (writePreppedFiles) write.csv(PE,"data/prepped/PE.csv",row.names=FALSE)
+log <- c(log,paste("Removing lake chains deleted",tmp-nrow(PE),
+                 "WBIC_YEAR PE values."))
 
 # Found unique wbic and wbic_year codes in the PE file.
 ## We must have a PE to estimate P and B. Thus, these wbic codes can be
@@ -61,7 +70,77 @@ if (writePreppedFiles) write.csv(PE,"data/prepped/PE.csv",row.names=FALSE)
 #  limit memory issues
 wbics <- unique(PE$wbic)
 wbic_years <- unique(PE$wbic_year)
-rm(PE)
+log <- c(log,paste("There are",length(wbic_years),"unique WBIC_YEARS and",
+                   length(wbics),"unique WBICs in PE.csv."))
+## Write out the file ... PE.csv
+if (writePreppedFiles) {
+  write.csv(PE,"data/prepped/PE.csv",row.names=FALSE)
+  log <- c(log,paste("Saved prepped PE.csv with",ncol(PE),"variables and",
+                   nrow(PE),"records.\n\n"))
+}
+rm(PE,tmp)
+
+
+
+
+# ======================================================================
+# Read and prep the WBIC characteristics file (ROW)
+## Reduced to only WBICs for which we have a PE
+## Selected only needed columns, renamed those columns
+##   Did not include area, lat, long, or depths as those values seemed
+##   more complete in the lake classification file that AR sent (below)
+## Converted mean depth to meters
+wbicInfo <- read.csv("data/original/ROW.csv")
+log <- c(log,paste("Loaded ROW file with",ncol(wbicInfo),"variables and",
+                   nrow(wbicInfo),"records."))
+wbicInfo %<>% filterD(WBIC %in% wbics) %>%
+  select(WBIC,OFFICIAL_NAME,WATERBODY_TYPE_DESC,MEAN_DEPTH_FT) %>%
+  rename(wbic=WBIC,name=OFFICIAL_NAME,wb_type=WATERBODY_TYPE_DESC,
+         mean_depth=MEAN_DEPTH_FT) %>%
+  mutate(mean_depth=mean_depth*0.3048)
+headtail(wbicInfo)
+
+## Read AR's lake classifications
+## Selected only needed columns, renamed those columns
+## Made lake class names simpler
+wbicInfo2 <- read.csv("data/original/Supplementary Dataset 8 - Final Lake Class List.csv",stringsAsFactors=FALSE)
+log <- c(log,paste("Loaded Rypel's Lake Class List file with",
+                   ncol(wbicInfo2),"variables and",nrow(wbicInfo2),"records."))
+wbicInfo2 %<>% select(WBIC,County,Area..ha.,Max.Depth..m.,
+                      Latitude,Longitude,Final.Lake.Class) %>%
+  rename(wbic=WBIC,county=County,size=Area..ha.,max_depth=Max.Depth..m.,
+         lat=Latitude,long=Longitude,class=Final.Lake.Class) %>%
+  mutate(class=mapvalues(class,
+                         from=c("Complex - Cool - Clear","Complex - Cool - Dark",
+                                "Complex - Riverine","Complex - Two Story",
+                                "Complex - Warm - Clear","Complex - Warm - Dark",
+                                "Simple - Cool - Clear","Simple - Cool - Dark",
+                                "Simple - Harsh - Has Fishery","Simple - Harsh - No Fishery",
+                                "Simple - Riverine","Simple - Trout Pond",
+                                "Simple - Two Story","Simple - Warm - Clear",
+                                "Simple - Warm - Dark"),
+                         to=c("CCC","CCD","CR","C2S","CWC","CWD","SCC","SCD",
+                              "SHF","SHN","SR","STP","S2S","SWC","SWD")))
+headtail(wbicInfo2)
+
+## Joined the county names and lake class variables to the ROW file
+## Rearranged columns and ordered rows by WBIC
+wbicInfo <- plyr::join(wbicInfo,wbicInfo2,by="wbic") %>%
+  select(wbic,name,county,class,wb_type,lat,long,size,max_depth,mean_depth) %>%
+  arrange(wbic)
+## Note that only 9 WBICs were in three "simple" lake classifications --
+##   collapsed these three classes into one called "SIM" for simple.
+xtabs(~class,data=wbicInfo)
+wbicInfo %<>% mutate(class=mapvalues(class,from=c("SCC","SWC","SWD"),
+                                     to=c("SIM","SIM","SIM")))
+xtabs(~class,data=wbicInfo)
+headtail(wbicInfo)
+## Write out the file ... wbicInfo.csv
+if (writePreppedFiles) {
+  write.csv(wbicInfo,"data/prepped/wbicInfo.csv",row.names=FALSE)
+  log <- c(log,paste("Saved prepped wbicInfo.csv with",ncol(wbicInfo),
+                     "variables and",nrow(wbicInfo),"records.\n\n"))
+}
 
 
 
@@ -70,26 +149,28 @@ rm(PE)
 ## Selected only needed columns, renamed those columns
 ## Reduced to only WBIC_YEARs for which we have a PE
 fmdb <- read.csv("data/original/walleye_raw_2012.csv",
-                 stringsAsFactors=FALSE,na.strings=c("-","NA","")) %>%
-  select(WBIC,Survey.Year,Length.or.Lower.Length.IN,Length.Upper.IN,
-         Number.of.Fish,Gender) %>%
+                 stringsAsFactors=FALSE,na.strings=c("-","NA",""))
+log <- c(log,paste("Loaded walleye FMDB file with",ncol(fmdb),
+                   "variables and",nrow(fmdb),"records."))
+fmdb %<>% select(WBIC,Survey.Year,Length.or.Lower.Length.IN,
+                 Length.Upper.IN,Number.of.Fish,Gender) %>%
   rename(wbic=WBIC,year=Survey.Year,sex=Gender) %>%
   mutate(wbic_year=paste(wbic,year,sep="_")) %>%
   filterD(wbic_year %in% wbic_years)
 ## Handling database errors or issues (per AR e-mail 1-Aug-17)
-## Don't use filterD() because it removes NAs which causes issues
+## Don't use filterD() because it removes NAs which causes issues!!!!!
 ## Removed rows where a number of fish is given, but no lengths
 rows2delete <- which(fmdb$Number.of.Fish>0 & is.na(fmdb$Length.or.Lower.Length.IN))
 if (length(rows2delete)>0) fmdb <- fmdb[-rows2delete,]
-cat("Deleted",length(rows2delete),"rows from the FMDB with fish but no length.\n")
+log <- c(log,paste("Deleted",length(rows2delete),"rows with fish but no length."))
 ## Removed rows with "lower" length greater than "upper" length
 rows2delete <- which(fmdb$Length.or.Lower.Length.IN>fmdb$Length.Upper.IN)
 if (length(rows2delete)>0) fmdb <- fmdb[-rows2delete,]
-cat("Deleted",length(rows2delete),"rows from the FMDB with lower length > upper length.\n")
+log <- c(log,paste("Deleted",length(rows2delete),"rows with lower length > upper length."))
 ### Removed rows with negative lengths
 rows2delete <- which(fmdb$Length.or.Lower.Length.IN<0 | fmdb$Length.Upper.IN<0)
 if (length(rows2delete)>0) fmdb <- fmdb[-rows2delete,]
-cat("Deleted",length(rows2delete),"rows from the FMDB with negative lengths.\n")
+log <- c(log,paste("Deleted",length(rows2delete),"rows with negative lengths."))
 ## Removed rows where a number of fish is given, but the lower length
 ## is zero and the upper length is positive. I think this is for fish
 ## that were counted as less than the upper length. Two of the upper
@@ -100,7 +181,8 @@ cat("Deleted",length(rows2delete),"rows from the FMDB with negative lengths.\n")
 rows2delete <- which(fmdb$Length.or.Lower.Length.IN==0 & 
                        fmdb$Length.Upper.IN>0 & fmdb$Number.of.Fish>0)
 if (length(rows2delete)>0) fmdb <- fmdb[-rows2delete,]
-cat("Deleted",length(rows2delete),"rows from the FMDB in a 'small fish' bin.\n")
+log <- c(log,paste("Deleted",length(rows2delete),
+                   "rows from the FMDB in a 'small fish' bin."))
 ## If no or zero numbers of fish but there are lengths then 
 ## assume there is one fish.
 fmdb$Number.of.Fish[is.na(fmdb$Number.of.Fish) & 
@@ -108,18 +190,30 @@ fmdb$Number.of.Fish[is.na(fmdb$Number.of.Fish) &
 fmdb$Number.of.Fish[fmdb$Number.of.Fish==0 & 
                       !is.na(fmdb$Length.or.Lower.Length.IN)] <- 1
 ## Generated individual lengths for fish recorded in length bins
+tmp <- capture.output(fmdb %<>% expandCounts(~Number.of.Fish,
+                         ~Length.or.Lower.Length.IN+Length.Upper.IN,
+                         new.name="len.in"),type="message")
+log <- c(log,tmp[-5])
 ## Added a length in mm variable
 ## Rearranged the columns and sorted the rows by wbic, year, and length
-fmdb %<>% expandCounts(~Number.of.Fish,
-                       ~Length.or.Lower.Length.IN+Length.Upper.IN,
-                       new.name="len.in") %>%
-  mutate(len.mm=len.in*25.4) %>%
+fmdb %<>% mutate(len.mm=len.in*25.4) %>%
   select(wbic_year,wbic,year,len.in,len.mm,sex,lennote) %>%
   arrange(wbic,year,len.mm)
-str(fmdb)
+## Join on the county and lake class variables
+##   Note that only 3 vars are kept in wbicInfo so that only county
+##     and class (and not mean_depth, etc.) will be added
+##   Note that plyr::join() must be used rather than dplyr::left_join()
+##     because of memory issues with left_join()
+fmdb <- plyr::join(fmdb,wbicInfo[,c("wbic","county","class")],by="wbic")
 headtail(fmdb)
-if (writePreppedFiles) write.csv(fmdb,"data/prepped/fmdb_WAE.csv",row.names=FALSE)
-rm(fmdb)
+## Write out the file ... fmdb_WAE.csv
+if (writePreppedFiles) {
+  write.csv(fmdb,"data/prepped/fmdb_WAE.csv",row.names=FALSE)
+  log <- c(log,paste("Saved prepped fmdb_WAE.csv with",ncol(fmdb),
+                     "variables and",nrow(fmdb),"records.\n\n"))
+}
+rm(fmdb,rows2delete,tmp)
+
 
 
 
@@ -131,10 +225,12 @@ rm(fmdb)
 ## Added length in mm and wbic-year combination variables
 ## Converted weight to numeric (was character because of commas)
 ## Rearranged columns and sorted rows by wbic, year, length, and age
-lwa <- read.csv("data/original/walleye_length_weight_age.csv",
-                stringsAsFactors=FALSE,na.strings=c("-","NA","")) %>%
-  select(WBIC,Survey.Year,Length.IN,Weight.Grams,
-         Age..observed.annuli.,Age.Structure.,Gender) %>%
+lwa <- read.csv("data/original/length_weight_age_raw_data_8_1_17.csv",
+                stringsAsFactors=FALSE,na.strings=c("-","NA",""))
+log <- c(log,paste("Loaded length_weight_age file with",
+                   ncol(lwa),"variables and",nrow(lwa),"records."))
+lwa %<>% select(WBIC,Survey.Year,Length.IN,Weight.Grams,
+                Age..observed.annuli.,Age.Structure.,Gender) %>%
   rename(wbic=WBIC,year=Survey.Year,sex=Gender,
          len.in=Length.IN,wt=Weight.Grams,
          age=Age..observed.annuli.,strux=Age.Structure.) %>%
@@ -144,52 +240,52 @@ lwa <- read.csv("data/original/walleye_length_weight_age.csv",
   filterD(wbic_year %in% wbic_years) %>%
   select(wbic_year,wbic,year,len.in,len.mm,wt,age,sex,strux) %>%
   arrange(wbic,year,len.mm,age)
-
+## Join on the county and lake class variables
+##   Note that only 3 vars are kept in wbicInfo so that only county
+##     and class (and not mean_depth, etc.) will be added
+## Rearranged columns
+lwa <- plyr::join(lwa,wbicInfo[,c("wbic","county","class")],by="wbic") %>%
+  select(wbic_year,wbic,year,county,class,len.in,len.mm,wt,age,sex,strux)
 # Isolate those fish that have both length and weight
 ## Remove age and strux variables
 lw <- filterD(lwa,!is.na(len.mm),!is.na(wt)) %>%
   select(-age,-strux)
-str(lw)
-headtail(lw)
-if (writePreppedFiles) write.csv(lw,"data/prepped/len_wt.csv",row.names=FALSE)
+log <- c(log,paste(nrow(lw),"fish had lengths and weights."))
+## Removed fish for which the weight variable was =0
+rows2delete <- which(lw$wt==0)
+if (length(rows2delete)>0) lw <- lw[-rows2delete,]
+log <- c(log,paste("Deleted",length(rows2delete),"rows with weights of zero."))
+## Removed fish for which the len.in variable was <1
+rows2delete <- which(lw$len.in<1)
+if (length(rows2delete)>0) lw <- lw[-rows2delete,]
+log <- c(log,paste("Deleted",length(rows2delete),"rows with a length (in) < 1."))
+## Removed fish for which the wt (in grams) variable was <1
+rows2delete <- which(lw$wt<1)
+if (length(rows2delete)>0) lw <- lw[-rows2delete,]
+log <- c(log,paste("Deleted",length(rows2delete),"rows with a weight (g) < 1."))
+## Write out the file ... len_wt.csv
+if (writePreppedFiles) {
+  write.csv(lw,"data/prepped/len_wt.csv",row.names=FALSE)
+  log <- c(log,paste("Saved prepped len_wt.csv with",ncol(lw),
+                     "variables and",nrow(lw),"records."))
+}
 
 # Isolate those fish that have both length and age
 ## Remove wt variable
 la <- filterD(lwa,!is.na(len.mm),!is.na(age)) %>%
   select(-wt)
-str(la)
-headtail(la)
-if (writePreppedFiles) write.csv(la,"data/prepped/len_age.csv",row.names=FALSE)
+log <- c(log,paste(nrow(la),"fish had lengths and ages."))
+## Write out the file ... len_age.csv
+if (writePreppedFiles) {
+  write.csv(la,"data/prepped/len_age.csv",row.names=FALSE)
+  log <- c(log,paste("Saved prepped len_age.csv with",ncol(la),
+                     "variables and",nrow(la),"records.\n\n"))
+}
 rm(lwa,lw,la)
+rm(wbicInfo,wbicInfo2,wbic_years,wbics)
 
-
-
-# ======================================================================
-# Read and prep the WBIC characteristics file (ROW)
-## Reduced to only WBICs for which we have a PE
-## Selected only needed columns, renamed those columns
-## Converted lake size and depths to metrics units (ha, m)
-## Rearranged columns and sorted rows by wbic
-## !! NEED TO CREATE SOME SORT OF REGION VARIABLE (for ALK and LW)
-ROW <- read.csv("data/original/ROW.csv") %>%
-  filterD(WBIC %in% wbics) %>%
-  select(WBIC,WBIC_TYPE_CODE,OFFICIAL_NAME,LOCAL_NAME,
-         OFFICIAL_SIZE_ACRES,MAX_DEPTH_FT,MEAN_DEPTH_FT,WATERBODY_TYPE_DESC,
-         CENTROID_LL_LAT_DD_AMT,CENTROID_LL_LONG_DD_AMT) %>%
-  rename(wbic=WBIC,wbic_type=WBIC_TYPE_CODE,name=OFFICIAL_NAME,
-         locname=LOCAL_NAME,wb_type=WATERBODY_TYPE_DESC,
-         size=OFFICIAL_SIZE_ACRES,max_depth=MAX_DEPTH_FT,
-         mean_depth=MEAN_DEPTH_FT,lat=CENTROID_LL_LAT_DD_AMT,
-         long=CENTROID_LL_LONG_DD_AMT) %>%
-  mutate(size=size*0.404686,max_depth=max_depth*0.3048,
-         mean_depth=mean_depth*0.3048) %>%
-  select(wbic,wbic_type,name,locname,wb_type,lat,long,size,max_depth,mean_depth) %>%
-  arrange(wbic)
-str(ROW)
-headtail(ROW)
-# which WBICs have PEs but nothing in ROW (can delete when this is fixed)
-wbics[which(!wbics %in% ROW$wbic)]
-if (writePreppedFiles) write.csv(ROW,"data/prepped/ROW.csv",row.names=FALSE)
-rm(ROW)
-
-
+# Write out log file
+logconn <- file(paste0("data/prepped/dataPrepper_logs/dataPrepperLog_",
+                       format(Sys.time(),"%d%b%Y_%H%M"),".txt"))
+writeLines(log,logconn)
+close(logconn)
