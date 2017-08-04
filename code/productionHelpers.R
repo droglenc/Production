af2e86cbbeb53a65f028a2cb915cbb26d95daa82
Derek Ogle <dogle@northland.edu>
2017-08-04 13:56:14 -0500

    Added getALK() to helpers files

--- code/productionHelpers.R
+++ code/productionHelpers.R
@@ -41,7 +41,7 @@ getSize <- function(df,WB,verbose=FALSE) {
 
 #' Identifies and returns appropriate weight-length regression.
 #' 
-#' This function takes a data.frame of one WBIC_YEAR ad identifies and returns the appropriate regression results. See workflow document for how "appropriate" is defined.
+#' This function takes a data.frame of one WBIC_YEAR and identifies and returns the appropriate regression results. See workflow document for how "appropriate" is defined.
 #' 
 #' @param df A data.frame filtered from the FMDB file for one WBIC_YEAR.
 #' @param LWRegs A data.frame of valid weight-length regressions filtered from the LWRegs file.
@@ -59,4 +59,34 @@ getLWReg <- function(df,LWRegs) {
   else row <- which(LWRegs$which=="ALL")
   # Return appropriate row
   LWRegs[row,]
+}
+
+
+
+#' Identifies and returns appropriate age-lengthe key.
+#' 
+#' This function takes a data.frame of one WBIC_YEAR and identifies and returns the appropriate age-length key. The user may choose either to use emprical or smoothed age-length keys. See workflow document for how "appropriate" is defined.
+#' 
+#' @param df A data.frame filtered from the FMDB file for one WBIC_YEAR.
+#' @param ALKInfo A data.frame of valid information about age-length keys filtered from the ALKInfo file.
+#' @param type A string that indicates whether to use "empirical" (observed) or "smoothed" (modeled) age=length keys.
+#'  
+getALK <- function(df,ALKInfo,type) {
+  # Find WBIC,YEAR,WBIC_YEAR, and CLASS
+  WBIC <- as.character(unique(df$wbic))
+  YEAR <- as.character(unique(df$year))
+  WBIC_YEAR <- paste(WBIC,YEAR,sep="_")
+  CLASS <- as.character(unique(df$class))
+  # Find the name of the ALK to use
+  if (WBIC_YEAR %in% ALKInfo$which) row <- which(ALKInfo$which==WBIC_YEAR)
+  else if (WBIC %in% ALKInfo$which) row <- which(ALKInfo$which==WBIC)
+  else if (CLASS %in% ALKInfo$which) row <- which(ALKInfo$which==CLASS)
+  else row <- which(ALKInfo$which=="ALL")
+  # Find appropriate alk
+  dir <- "data/prepped/ALKs/"
+  ext <- ".RData"
+  nm <- ifelse(type=="empirical",ALKInfo$ename[row],ALKInfo$sname[row])
+  load(paste0(dir,nm,ext))
+  # Return a list that has the ALK and some other information
+  list(ALK=alk,which=ALKInfo$which[row],type=ALKInfo$type[row])
 }
