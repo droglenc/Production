7e7942d746f65c7935c4771f08d24e517459a082
Derek Ogle <dogle@northland.edu>
2017-08-03 20:54:52 -0500

    Move old OneExample and started another

--- scratch/OneExample.R
+++ scratch/OneExample.R
@@ -16,54 +16,69 @@ library(FSA)
 library(dplyr)
 # Source local file
 source("code/calcPB.R")
+source("code/productionHelpers.R")
 
 
 
+# ======================================================================
 # Load the data.frames and do initial wranglings
-## Population estimates
-pe <- read.csv("data/prepped/PE.csv")
-## Load WBIC characteristics data
-ROW <- read.csv("data/prepped/ROW.csv")
-## Load length-age data
-###  Created 20-mm length categories (from pseudocode doc)
-###  Filtered to just age-3 and older, Removed unknown sexed fish
-lenAge <- read.csv("data/prepped/len_age.csv") %>%
-  mutate(lcat=lencat(len.mm,w=20)) %>%
-  filterD(age>=3,sex %in% c("F","M"))
-## Load length-weight data
-###  Added logged versions of length and weight
-###  Removed unknown sexed fish
-lenWt <- read.csv("data/prepped/len_wt.csv") %>%
-  mutate(logl=log(len.mm),logw=log(wt)) %>%
-  filterD(sex %in% c("F","M"))
+## Load WBIC characteristics
+wbic <- read.csv("data/prepped/wbicInfo.csv",stringsAsFactors=FALSE)
 ## Load length data
-fmdb <- read.csv("data/prepped/fmdb_WAE.csv")
+fmdb <- read.csv("data/prepped/fmdb_WAE.csv",stringsAsFactors=FALSE)
+## Population estimates
+### Removed WBIC_YEARs for which no FMDB data existed
+pe <- read.csv("data/prepped/PE.csv")
+pe <- pe[which(pe$wbic_year %in% unique(fmdb$wbic_year)),]
+## Load weight-length regression results
+### Only retain regressions results that are valid to use
+### Remove variables that defined use and reason for not using
+LWRegs <- read.csv("data/prepped/LWregs.csv",stringsAsFactors=FALSE) %>%
+  filterD(use=="yes") %>%
+  select(-use,-reason)
+## Load age-length-key results
 
 
 
+
+
+# ======================================================================
+## Lists all WBIC_YEARS in PE, AL, and LW files - JUST FOR TESTING
+wys <- as.character(pe$wbic_year)
 # Choose a wbic 
-WBIC <- 2353600
-# Choose a year
-pe$year[grepl(WBIC,pe$wbic)]
-YEAR <- 2002
-WBIC_YEAR <- paste(WBIC,YEAR,sep="_")
+( WBIC_YEAR <- wys[601] )
 
-# Isolate PE for that WBIC_YEAR
-( pe_1 <- filterD(pe,wbic_year==WBIC_YEAR)$PE )
-# Isolate lake size (ha) for that WBIC
-( area_1 <- filterD(ROW,wbic==WBIC)$size )
-# Create ALK
-lenAge_1 <- filterD(lenAge,wbic_year==WBIC_YEAR)
-alk <- prop.table(xtabs(~lcat+age,data=lenAge_1),margin=1)
-# Create LW regression
-lenWt_1 <- filterD(lenWt,wbic_year==WBIC_YEAR)
-lw <- lm(logw~logl,data=lenWt_1)
-# Isolate length data, add ages from ALK, predict weights
-fmdb_1 <- filterD(fmdb,wbic_year==WBIC_YEAR) %>%
-  alkIndivAge(alk,~len.mm,data=.) %>%
-  mutate(wt=exp(predict(lw,data.frame(logl=log(len.mm)))))
-# Examine data.frame
-head(fmdb_1)
+# Isolate length data
+fmdb_1 <- filterD(fmdb,wbic_year==WBIC_YEAR)
+# Find WL regression, Predict and add weights
+tmp <- getLWReg(fmdb_1,LWRegs)
+fmdb_1 %<>% mutate(wt=tmp$a*len.mm^tmp$b)
+
+## Put in a loop to see if it errors
+ttl.wys <- length(wys)
+reg.src <- character(ttl.wys)
+reg.type <- character(ttl.wys)
+for (i in 1:ttl.wys) {
+  print(paste0("i=",i))
+  fmdb_1 <- filterD(fmdb,wbic_year==wys[i])
+  tmp <- getLWReg(fmdb_1,LWRegs)
+  fmdb_1 %<>% mutate(wt=tmp$a*len.mm^tmp$b)
+  reg.src[i] <- tmp$which
+  reg.type[i] <- tmp$type
+}
+
+xtabs(~reg.type)
+# Predict and add ages (use alkIndivAge, but need to find appropriate ALK)
+#fmdb_1 %<>%
+  
+#alkIndivAge(alk,~len.mm,data=.)
+
+
+getPE(fmdb_1,pe,verbose=TRUE)
+
+getSize(fmdb_1,wbic,verbose=TRUE)  
+  
+
 
 # Summarize
 ## get number and mean weight (kg) in each age-class
@@ -72,8 +87,8 @@ head(fmdb_1)
 ## send to calcPB to compute P and B
 sum_1 <- group_by(fmdb_1,age) %>%
   summarize(num=n(),mwt=mean(wt)/1000) %>%
-  mutate(num=num/sum(num)*pe_1,twt=num*mwt) %>%
-  calcPB(age.c=1,num.c=2,twt.c=4,area=area_1)
+  mutate(num=num/sum(num)*getPE(fmdb_1,pe),twt=num*mwt) %>%
+  calcPB(age.c=1,num.c=2,twt.c=4,area=getSize(fmdb_1,wbic))
 round(sum_1$df,2)
 sum_1$PperA
 sum_1$BperA
@@ -82,11 +97,3 @@ sum_1$BperA
 
 
 
-## Lists all WBIC_YEARS in PE, AL, and LW files
-wys <- as.character(pe$wbic_year)
-wys_al <- as.character(unique(lenAge$wbic_year))
-wys_lw <- as.character(unique(lenWt$wbic_year))
-
-wys_all <- wys[wys %in% wys_al]
-wys_all <- wys_all[wys_all %in% wys_lw]
-wys_all
