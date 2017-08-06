b526b1ae11896fa2189beeb9182f019ea8c6f063
Derek Ogle <dogle@northland.edu>
2017-08-06 10:01:18 -0500

    Modified calcPB_loop (prepping for calcPB_loop2)

--- code/calcPB_loop.R
+++ code/calcPB_loop.R
@@ -1,5 +1,10 @@
 ## Clear workspace and console
 rm(list=ls()); cat("\014")
+# Load required packages
+library(FSA); library(dplyr)
+# Source local helper files
+source("code/helpers/calcPB.R")
+source("code/helpers/productionHelpers.R")
 
 # ######################################################################
 # ======================================================================
@@ -15,13 +20,15 @@ rm(list=ls()); cat("\014")
 # ######################################################################
 
 # ======================================================================
-# Setup
-## Load required packages
-library(FSA)
-library(dplyr)
-# Source local file
-source("code/helpers/calcPB.R")
-source("code/helpers/productionHelpers.R")
+# Analysis choices that can be made (at this stage)
+## Minimum size of sample when computing P & B
+n.cut <- 30
+## Minimum number of ages in sample when computing P & B
+ages.cut <- 5
+## Type of ALK to use ("empirical" or "smoothed")
+alk2use <- "smoothed"
+## A name to add as a suffix to the results file (if NULL, use date-time)
+results.suffix <- NULL
 
 
 
@@ -38,47 +45,54 @@ rows2delete <- which(!pe$wbic_year %in% unique(fmdb$wbic_year))
 pe <- pe[-rows2delete,]
 cat(length(rows2delete),"WBIC_YEARs were removed from PE data.frame
     because no matching data in FMDB data.frame.")
-## Load weight-length regression results
-### Only retain regressions results that are valid to use
-### Remove variables that defined use and reason for not using
-LWRegs <- read.csv("data/prepped/LWregs.csv",stringsAsFactors=FALSE) %>%
-  filterD(use=="yes") %>%
-  select(-use,-reason)
 ## Load age-length-key information
 ### Only retain information for ALKs that are valid to use
 ### Retain only variables that are need when using the ALK
 ALKInfo <- read.csv("data/prepped/ALKInfo.csv",stringsAsFactors=FALSE) %>%
   filterD(use=="yes") %>%
   select(type,which,ename,sname)
+## Load weight-length regression results
+### Only retain regressions results that are valid to use
+### Remove variables that defined use and reason for not using
+LWRegs <- read.csv("data/prepped/LWregs.csv",stringsAsFactors=FALSE) %>%
+  filterD(use=="yes") %>%
+  select(-use,-reason)
 
 
 
 # ======================================================================
-## Put in a loop
+# Compute P & B (et al.) for each WBIC_YEAR
+## Prepare for loop
 wys <- as.character(pe$wbic_year)
 ttl.wys <- length(wys)
 reg.src <- reg.type <- alk.src <- alk.type <- alk.note <- character(ttl.wys)
 PE <- HA <- numAges <- n <- P <- B <- numeric(ttl.wys)
+## Loop through each WBIC_YEAR
 for (i in 1:ttl.wys) {
   print(paste0("i=",i," of ",ttl.wys," (",wys[i],")"))
   fmdb_1 <- filterD(fmdb,wbic_year==wys[i])
   # Get WBIC_YEAR PE and WBIC size
   PE[i] <- getPE(fmdb_1,pe)
   HA[i] <- getSize(fmdb_1,wbic)
-  tmp <- doLWReg(fmdb_1,LWRegs)
+  # Apply ALKs
+  tmp <- doALK(fmdb_1,ALKInfo,alk2use)
   fmdb_1 <- tmp$df
-  reg.src[i] <- tmp$reg$which; reg.type[i] <- tmp$reg$type
-  tmp <- doALK(fmdb_1,ALKInfo,"smoothed")
-  fmdb_1 <- tmp$df
-  alk.src[i] <- tmp$which; alk.type[i] <- tmp$type
-  alk.note[i] <- tmp$note
-  # Calculate P and B
+  alk.src[i] <- tmp$which; alk.type[i] <- tmp$type; alk.note[i] <- tmp$note
+  # Apply LWRegs (must check fmdb_1 as it may have no fish after ALK)
+  if (nrow(fmdb_1)>0) {
+    tmp <- doLWReg(fmdb_1,LWRegs)
+    fmdb_1 <- tmp$df
+    reg.src[i] <- tmp$reg$which; reg.type[i] <- tmp$reg$type    
+  }
+  # Get overall sample size
+  n[i] <- nrow(fmdb_1)
+  # Get number of ages present
+  numAges[i] <- length(unique(fmdb_1$age))
+  # Calculate P and B (if more than one age-class)
   ## get number and mean weight (kg) in each age-class
   ## expand number in sample to number in popn (with PE value)
   ## add a total weight
   ## send to calcPB to compute P and B
-  n[i] <- nrow(fmdb_1)
-  numAges[i] <- length(unique(fmdb_1$age))
   if (numAges[i]>1) {
     sum_1 <- group_by(fmdb_1,age) %>%
       summarize(snum=n(),mwt=mean(wt)/1000) %>%
@@ -86,7 +100,7 @@ for (i in 1:ttl.wys) {
       calcPB(age.c="age",num.c="pnum",twt.c="twt",area=HA[i],adjAgeGaps=TRUE)
     P[i] <- sum_1$PperA
     B[i] <- sum_1$BperA
-    ## Print out the calculation table so it can be examined later
+    ## Write out the calculation table so it can be examined later
     write.csv(sum_1$df,paste0("results/CalcPB_Tables/PB_",wys[i],".csv"),
               quote=FALSE,row.names=FALSE)
   } else P[i] <- B[i] <- NA
@@ -94,13 +108,12 @@ for (i in 1:ttl.wys) {
 
 
 # ======================================================================
-## Put Results together
+# Put Results together
 split.wy <- do.call(rbind,strsplit(wys,"_"))
 PB_res <- data.frame(wbic_year=wys,wbic=split.wy[,1],year=split.wy[,2],
                      PE,HA,n,numAges,P,B,
                      reg.src,reg.type,alk.src,alk.type,alk.note)
 ## Add use and reason variables
-n.cut <- 30; ages.cut <- 5
 PB_res %<>% mutate(use=case_when(n<n.cut ~ "NO",
                                  numAges<ages.cut ~ "NO",
                                  TRUE ~ "yes"),
@@ -108,5 +121,6 @@ PB_res %<>% mutate(use=case_when(n<n.cut ~ "NO",
                                     numAges<ages.cut ~ paste0("numAges<",ages.cut),
                                     TRUE ~ "yes"))
 ## Write the file out to the results folder with a time stamp in name
-write.csv(PB_res,paste0("results/PB_",format(Sys.time(),"%d%b%Y_%H%M"),".csv"),
+if (is.null(results.suffix)) results.suffix <- format(Sys.time(),"%d%b%Y_%H%M")
+write.csv(PB_res,paste0("results/PB_",results.suffix,".csv"),
           quote=FALSE,row.names=FALSE)
