1b1ce7d2dea2985b54b9e8f41fdf0bcbe4f705fa
Derek Ogle <dogle@northland.edu>
2017-08-08 23:22:57 -0500

    Added more helper functions

--- code/helpers/productionHelpers.R
+++ code/helpers/productionHelpers.R
@@ -118,3 +118,40 @@ makeNote <- function(note,addlnote) {
   else note <- paste(note,addlnote)
   note
 }
+
+
+handleLostWBIC_YEARs <- function(oldlog,oldWYs,df) {
+  newWYs <- unique(df$wbic_year)
+  if (length(oldWYs)==length(newWYs)) tmp <- "  (no WBIC_YEARs lost)"
+  else {
+    lostWYs <- oldWYs[!oldWYs %in% newWYs]
+    ## do this so that they can be ordered
+    tmp <- do.call(rbind,strsplit(lostWYs,"_"))
+    tmp <- data.frame(wbic_year=lostWYs,wbic=as.numeric(tmp[,1]),
+                      year=as.numeric(tmp[,2]),stringsAsFactors=FALSE) %>%
+      arrange(wbic,year)
+    tmp <- paste0("  (lost WBIC_YEARs: ",paste(tmp$wbic_year,collapse=", "),")")
+  }
+  c(oldlog,tmp)
+}
+
+
+## Extract some information from the lists returned by lmList that
+## contains each length-weight regression by group
+LWINFO <- function(lms,type) {
+  ##  Got summary of each regression ... this is a BIG list
+  tmp <- lapply(lms,summary)
+  ##  Got model.frame for each regression ... to get min and max len below
+  tmp2 <- sapply(lapply(lms,model.frame),'[[','logl')
+  ##  Extracted coefficients, r-squared, and sample size (df_reg+2)
+  ##  Renamed coefficients
+  res <- data.frame(coef(lms),rsq=sapply(tmp,'[[','r.squared'),
+                    n=sapply(tmp,'[[','df')[2,]+2,
+                    minLen=exp(sapply(tmp2,min)),
+                    maxLen=exp(sapply(tmp2,max))) %>%
+    rename(loga=X.Intercept.,b=logl)
+  ##  Added 'which' variable that lists the group/level for the regression
+  ##  Added 'type' variable for level of regression (e.g., at WBIC_YEAR)
+  res %<>% mutate(which=rownames(res),type=type)
+  res
+}
